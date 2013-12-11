aa980c30fb05b134f85fddeee12343934b503b57
Cynerva <cynerva@gmail.com>
2013-12-11 17:44:44 -0600

    Added mmpz2xml script

--- pymmpz/mmpz.py
+++ pymmpz/mmpz.py
@@ -4,7 +4,7 @@ from xml.etree import ElementTree
 def get_xml(filename):
     contents = open(filename, "rb").read()
     xml = zlib.decompress(contents[4:])
-    return xml
+    return xml.strip()
 
 def get_element_tree(filename):
     return ElementTree.fromstring(get_xml(filename))
--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name="pymmpz",
     version="0.0.0",
-    packages=["pymmpz"]
+    packages=["pymmpz"],
+    scripts=["scripts/mmpz2xml"]
 )

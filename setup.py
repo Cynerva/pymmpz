308841378aed62bf432800d803302229097383a3
Cynerva <cynerva@gmail.com>
2013-12-11 19:27:27 -0600

    Added mmpz2midi script

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="pymmpz",
     version="0.0.0",
     packages=["pymmpz"],
-    scripts=["scripts/mmpz2xml"],
+    scripts=["scripts/mmpz2xml", "scripts/mmpz2midi"],
     install_requires=["midiutil"]
 )

4e60f83a2b9f037d6e5f1f365c5485d6d1324a43
Cynerva <cynerva@gmail.com>
2013-12-11 20:39:25 -0600

    Add tempo events from automation tracks

--- pymmpz/mmpz.py
+++ pymmpz/mmpz.py
@@ -8,14 +8,26 @@ def get_xml(filename):
     xml = zlib.decompress(contents[4:])
     return xml.strip()
 
+
 def get_root(filename):
     return ElementTree.fromstring(get_xml(filename))
+
+
+def get_starting_tempo(root):
+    head = root.find("head")
+    try:
+        return int(head.get("bpm"))
+    except TypeError:
+        # Some versions of mmpz have BPM as an element, not an attrib.
+        return int(head.find("bpm").get("value"))
+
 
 def get_instrument_count(element):
     count = 0
     for _ in element.iter("instrument"):
         count += 1
     return count
+
 
 def add_notes_from_pattern(midifile, pattern, track=0, time=0):
     # 16 channels to choose from, but channel 9 is percussion, so skip it
@@ -24,7 +36,7 @@ def add_notes_from_pattern(midifile, pattern, track=0, time=0):
         channel += 1
 
     time += int(pattern.get("pos"))
-    for note in pattern.iter("note"):
+    for note in pattern.iterfind("note"):
         midifile.addNote(
             track=track,
             channel=channel,
@@ -34,12 +46,17 @@ def add_notes_from_pattern(midifile, pattern, track=0, time=0):
             volume=int(note.get("vol"))
         )
 
-def get_starting_tempo(root):
-    head = root.find("head")
-    try:
-        return int(head.get("bpm"))
-    except TypeError:
-        return int(head.find("bpm").get("value"))
+
+def add_tempo_events_from_track(midifile, track):
+    for pattern in track.iterfind("automationpattern"):
+        time = int(pattern.get("pos"))
+        for event in pattern.iterfind("time"):
+            midifile.addTempo(
+                track=0,
+                time=(time + int(event.get("pos"))) / 48.0,
+                tempo=int(event.get("value"))
+            )
+
 
 def convert_mmpz_to_midi(infile, outfile):
     root = get_root(infile)
@@ -56,5 +73,9 @@ def convert_mmpz_to_midi(infile, outfile):
         elif track.get("type") == "1":
             # Beat/Bassline
             pass # TODO
+        elif track.get("type") == "5":
+            # Automation track
+            add_tempo_events_from_track(midifile, track)
+                    
 
     midifile.writeFile(open(outfile, "wb"))
